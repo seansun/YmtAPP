c95a06e911f7810c697c2eaccfa26c12fbcb90bd
seansun <sgsg1986@126.com>
2017-07-03 11:04:46 +0800

    u

---
+++ src/main/java/com/ymt/engine/IOSEngine.java
@@ -0,0 +1,19 @@
+package com.ymt.engine;
+
+/**
+ * Created by sunsheng on 2017/6/27.
+ */
+public class IOSEngine extends Engine {
+
+
+
+    /**
+     * 截图
+     */
+    @Override
+    public void screenShot(String fileName) {
+
+       // TODO: 2017/6/27   ios 截图实现
+
+    }
+}
